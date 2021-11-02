e4faba30802544b1feae1194a200b91fe4853685
hokmyn <den_from_bykhov1988@mail.ru>
2021-11-02 20:26:29 +0300

    feat: add solution to copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require("fs");
+const fsPromises = require("fs").promises;
 
+const pathToCopy = `${__dirname}/files`;
+
+fsPromises
+  .mkdir(`${__dirname}/files-copy`, { recursive: true })
+  .then((result) => {
+    fs.readdir(pathToCopy, (err, files) => {
+      files.forEach((item) => {
+        fsPromises
+          .copyFile(`${pathToCopy}/${item}`, `${__dirname}/files-copy/${item}`)
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
