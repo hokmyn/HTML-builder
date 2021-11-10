ae08d97072200de50c1badd751f1d9828f030dfb
Denis <den_from_bykhov1988@mail.ru>
2021-11-11 00:52:39 +0300

    Update index.js

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,6 +6,15 @@ const pathToCopy = `${__dirname}/files`;
 fsPromises
   .mkdir(`${__dirname}/files-copy`, { recursive: true })
   .then((result) => {
+    if (result == undefined) {
+      fs.readdir(`${__dirname}/files-copy`, (err, files) => {
+        files.forEach((item) => {
+          fs.unlink(`${__dirname}/files-copy/${item}`, (err) => {
+            if (err) throw err;
+          });
+        });
+      });
+    }
     fs.readdir(pathToCopy, (err, files) => {
       files.forEach((item) => {
         fsPromises
